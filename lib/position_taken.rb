860c24d9f7c8d2c8de8939625f1b455100104aaf
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-01-15 07:12:22 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,8 @@
 def valid_move?(board, index)
-  if index.between?(0,8) && position_taken
+  if index.between?(0,8) && position_taken? == true
+else index.between?(0,8) || position_taken? == nil
+end
+end
 
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
